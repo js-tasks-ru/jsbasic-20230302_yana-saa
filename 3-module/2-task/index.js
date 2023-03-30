669974c4813a0e66680f25f79ad952303167b1a1
yanasaa <yanasaa@gmail.com>
2023-03-30 16:24:09 +0300

    filteredRanged added

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,12 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let copiedArr = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+
+    if (arr[i] >= a && arr[i] <= b) {
+      copiedArr.push(arr[i]);
+    }
+  }
+
+  return copiedArr;
 }
