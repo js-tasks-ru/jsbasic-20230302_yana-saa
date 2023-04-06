bba76cc69bc568ec1797b3abfbb5b89516051284
yanasaa <yanasaa@gmail.com>
2023-04-06 19:06:13 +0300

    function getMinMax added

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
+  let obj = {};
+  let arr = str.split(' ');
+  let sortedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = +arr[i];
+    if (arr[i]) {
+      sortedArr.push(arr[i])
+    }
+  }
+  console.log(sortedArr)
+  obj.min = Math.min.apply(null, sortedArr);
+  obj.max = Math.max.apply(null, sortedArr);;
+  return obj;
 }
